6279222056ee361d8298e89ba6c19e2180250325
Koen De Groot <koen.degroot@organimmo.be>
2025-08-14 10:24:09 +0200

    refactor: remove medium and rectangular shapes from AnimatedBackground component for simplification

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -13,21 +13,11 @@ const AnimatedBackground = () => {
         <div className="shape circle large shape-2"></div>
         <div className="shape circle large shape-3"></div>
 
-        {/* Medium circles */}
-        <div className="shape circle medium shape-4"></div>
-        <div className="shape circle medium shape-5"></div>
-        <div className="shape circle medium shape-6"></div>
-
         {/* Small circles */}
         <div className="shape circle small shape-7"></div>
         <div className="shape circle small shape-8"></div>
         <div className="shape circle small shape-9"></div>
         <div className="shape circle small shape-10"></div>
-
-        {/* Rectangular shapes */}
-        <div className="shape rectangle rect-1"></div>
-        <div className="shape rectangle rect-2"></div>
-        <div className="shape rectangle rect-3"></div>
 
         {/* Hexagonal shapes */}
         <div className="shape hexagon hex-1"></div>
